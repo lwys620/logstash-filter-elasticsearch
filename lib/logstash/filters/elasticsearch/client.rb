1389c51a1df981785946cac7fffd241e48ddc9b7
lwys620 <weili.ru@gmail.com>
2016-11-02 15:18:51 -0400

    Update client.rb
    previous version of code, the port value was set to "https" instead of port numbers extracted from hosts parameter.   This causes "ConnectionFailed" error as it queries against __full_url like "https://https://hostname.domainname:9200:https".
    
    The change is to break down hosts parameters into different url section and set port, protocol parameters explicitly.
    
    Thanks,

--- lib/logstash/filters/elasticsearch/client.rb
+++ lib/logstash/filters/elasticsearch/client.rb
@@ -19,7 +19,52 @@ module LogStash
           transport_options[:headers] = { Authorization: "Basic #{token}" }
         end
 
-        hosts.map! {|h| { host: h, scheme: 'https' } } if ssl
+module LogStash
+  module Filters
+    class ElasticsearchClient
+
+      attr_reader :client
+
+      def initialize(user, password, options={})
+        ssl     = options.fetch(:ssl, false)
+        hosts   = options[:hosts]
+        @logger = options[:logger]
+
+        transport_options = {}
+        if user && password
+          token = ::Base64.strict_encode64("#{user}:#{password.value}")
+          transport_options[:headers] = { Authorization: "Basic #{token}" }
+        end
+        
+        # validate hosts parameters format if ssl is set
+        # and populate hosts array with correct protocol/port/scheme
+        if ssl  
+            ohosts = hosts
+            hosts = []
+            for h in ohosts
+                if not h =~ /https.*:[0-9]+/
+                    puts "Elasticsearch filter hosts parameter should be in this format if ssl is set to true: hosts => ['https://hostname:port']"
+                    exit
+                end   
+                h=h.gsub('/','')
+                elements=h.split(":")
+                hosts << {:host => elements[1], :scheme=> 'https', :protocol=> 'https', :port=> elements[2]}
+            end  
+        end
+
+        transport_options[:ssl] = { ca_file: options[:ca_file] } if ssl && options[:ca_file]
+
+        @logger.info("New ElasticSearch filter", :hosts => hosts)
+        @client = ::Elasticsearch::Client.new(hosts: hosts, transport_options: transport_options)
+      end
+
+      def search(params)
+        @client.search(params)
+      end
+
+    end
+  end
+end
         transport_options[:ssl] = { ca_file: options[:ca_file] } if ssl && options[:ca_file]
 
         @logger.info("New ElasticSearch filter", :hosts => hosts)
